a3ba824b82adcde992984147cce943b820325255
Yasuhiro Katayama <yasucom0406@gmail.com>
2019-10-03 08:41:52 -0700

    Delete dev.js

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://yasu4646:yk908447@cluster0-kbh4s.mongodb.net/find?retryWrites=true&w=majority',
-    cookieKey: 'kkjdjkjkjkdjkjkjdkkJKJKJKJDKKND1223333'
-}
