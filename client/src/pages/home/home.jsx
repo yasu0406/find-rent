aca7bc82ce1a7e981e664b1db5b11b327fe8e4c4
yasuhiro <yasucom4646@gmail.com>
2019-10-03 17:49:54 -0700

    Added RoomListAllCategory and RoomListCategory and Deleted mapStateToProps

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,51 +1,24 @@
-import React, { useState, useEffect }  from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import { ftechRooms } from '../../redux/rooms/rooms.action';
+import React, { useState }  from 'react';
+import { Route, withRouter } from 'react-router-dom';
 
+import RoomListAllCategory from '../../components/room-list/roomlist-all-category.component';
+import RoomListCategory from '../../components/room-list/roomlist-category.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
 import './home.styles.scss';
 
-
-
 const Home = props => {
-    const [areaCategoryName, setAreaCategoryName] = useState('downtown')
+    const [areaCategoryName, setAreaCategoryName] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.history.push(`/${areaCategoryName}`);
+        props.history.push(`category/${areaCategoryName}`);
     }
-
-    const test = () => {
-        if(props.rooms.rooms) {
-           return props.rooms.rooms.map((room) => {
-                const title = room.title;
-				const sliceTitle = title.length > 30 ? title.slice(0, 30) + '…' : title;
-                return (
-                    <li className='col-md-3' key={room._id}>
-                        <Link to={`room-detail/${room._id}`}>
-                            <div className='thumb-nail' style={{backgroundImage: `url(${room.img1})`}}></div>
-                            <p>{room.roomSize}</p>
-                            <h3>{sliceTitle}</h3>
-                            <p>${room.price}</p>
-                        </Link>
-                    </li>
-                )
-            })
-        }
-        
-    }
-
-    useEffect(() => {
-        props.ftechRooms();
-    },[]) 
-
     return (
         <>
         <div className='main-visual'>
             <div className='container'>
                 <div className='contact-form'>
-                <h2 className='mb-3'>Find Rooms</h2>
+                <h2>Find Rooms</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="exampleFormControlSelect1">Select Area</label>
@@ -66,43 +39,10 @@ const Home = props => {
                 </div>
             </div>
         </div>
-        {/* <Route exact path='/' component={RoomList} />
-        <Route exact path={`/${areaCategoryName}`} component={AreaCategoryList} /> */}
-        <section className='mt-5 container'>
-        <h2 className='mb-3'>Downtown</h2>
-        <ul className='row mb-5'>
-            {test()}
-        </ul>
-        <button className='btn btn-danger'>View</button>
-      </section>
-
-      <section className='mt-5 container'>
-        <h2 className='mb-3'>Robson</h2>
-        <ul className='row mb-5'>
-        </ul>
-        <button className='btn btn-danger'>View</button>
-      </section>
-
-      <section className='mt-5 container'>
-        <h2 className='mb-3'>Downtown</h2>
-        <ul className='row mb-5'>
-        </ul>
-        <button className='btn btn-danger'>View</button>
-      </section>
+        <Route exact path='/' component={RoomListAllCategory} />
+        <Route exact path='/category/:category' component={() => <RoomListCategory areaCategoryName={areaCategoryName} />} />
         </>
     );
 }
 
-const mapStateToProps = state => {
-    return {
-        rooms: state.rooms
-    }
-}
-
-
-export default connect(
-    mapStateToProps,
-    {
-        ftechRooms
-    }
-)(Home)
+export default withRouter(Home);
