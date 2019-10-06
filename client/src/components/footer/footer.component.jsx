efc2dee1eb2735a9520497f17112fd48b3fb9ca2
yasuhiro <yasucom4646@gmail.com>
2019-10-06 03:09:08 -0700

    Update footer.component.jsx

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -8,7 +8,7 @@ const Fotter = () => (
 		<div className="container">
 			<div className="row">
 				<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p className="h6">&copy All right Reversed.<a className="text-green ml-2" href="" target="_blank">Find</a></p>
+					<p className="h6">&copy All right Reversed.<a className="ml-2" href="" target="_blank">Find</a></p>
 				</div>
 			</div>	
 		</div>
