d8a03566deee72964ea22158c4f2402b0901f247
yasuhiro <yasucom4646@gmail.com>
2019-10-07 17:25:20 -0700

    Added bg-img for slideshow

--- client/src/pages/room-detail/room-detail.component.jsx
+++ client/src/pages/room-detail/room-detail.component.jsx
@@ -28,63 +28,73 @@ const RoomDetail = (props) => {
             <div>
                 <div className='carousel-wrap'>
                <Carousel showArrows={true} autoPlay={true} emulateTouch={true} stopOnHover={false} transitionTime={1000} infiniteLoop={true} interval={5000}>
-                <div style={{backgroundImage: `url(${img1})`}}>
-                    <img src={img1} alt={title}/>
+                <div style={{backgroundImage: `url(${img1 ? img1: process.env.PUBLIC_URL + '/images/default-img.png'})`}}>
+                    <img src={img1 ? img1: process.env.PUBLIC_URL + '/images/default-img.png'} alt={title}/>
                 </div>
-                <div style={{backgroundImage: `url(${img2})`}}>
-                    <img src={img2} alt={title} />
+                <div style={{backgroundImage: `url(${img2 ? img2: process.env.PUBLIC_URL + '/images/default-img.png'})`}}>
+                    <img src={img2 ? img2: process.env.PUBLIC_URL + '/images/default-img.png'} alt={title} />
                 </div>
-                <div style={{backgroundImage: `url(${img3})`}}>
-                    <img src={img3} alt={title} />
+                <div style={{backgroundImage: `url(${img3 ? img3: process.env.PUBLIC_URL + '/images/default-img.png'})`}}>
+                    <img src={img3 ? img3: process.env.PUBLIC_URL + '/images/default-img.png'} alt={title} />
                 </div>
-                <div style={{backgroundImage: `url(${img4})`}}>
-                    <img src={img4} alt={title} />
+                <div style={{backgroundImage: `url(${img4 ? img4: process.env.PUBLIC_URL + '/images/default-img.png'})`}}>
+                    <img src={img4 ? img4: process.env.PUBLIC_URL + '/images/default-img.png'} alt={title} />
                 </div>
-                <div style={{backgroundImage: `url(${img5})`}}>
-                    <img src={img5} alt={title} />
+                <div style={{backgroundImage: `url(${img5 ? img5: process.env.PUBLIC_URL + '/images/default-img.png'})`}}>
+                    <img src={img5 ? img5: process.env.PUBLIC_URL + '/images/default-img.png'} alt={title} />
                 </div>
             </Carousel>
                 </div>
             </div>
-            <div className='container'>
+            <div className='container room-info'>
                 <section>
                     <div className='row'>
                         <div className='col-md-10'>
                             <h2>{title}</h2>
                             <p>{area}</p>
                         </div>
-                        <div className='col-md-2'>
+                        <div className='col-md-2 user-avator'>
+                            <img src={userInfo.userPhoto? userInfo.userPhoto : `${process.env.PUBLIC_URL}/images/user-default-img.png`} alt={userInfo.userName}/>
                             <p>{userInfo.userName}</p>
                         </div>
                     </div>
-                    <dl>
-                        <dt>
-                            Private room in apartment
-                        </dt>
-                        <dd>
-                            {roomSize}
-                        </dd>
-                    </dl>
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>Monmouth Ave</dd>
-                    </dl>
-                    <hr/>
+                    <div>
+                        <dl>
+                            <dt>
+                                Private room in apartment
+                            </dt>
+                            <dd>
+                                {roomSize}
+                            </dd>
+                        </dl>
+                        <dl>
+                            <dt>Location</dt>
+                            <dd>Monmouth Ave</dd>
+                        </dl>
+                    </div>
+                </section>
+                    
+                <section className='room-describe'>    
                     <div>
                         <h2>The Room</h2>
                         <p>{describe}</p>
                         <p><a className='green-link' href={`mailto:${userInfo.userEmail}`}>Contact host</a></p>
                     </div>
-                    <hr/>
+                </section>
+                    
+                <section>
                     <div>
                         <h2>Amenities</h2>
 
                     </div>
-                    <hr/>
+                </section>    
+                
+                <section>
                     <div>
                         <h2>The neighborhood</h2>
+
                     </div>
-                </section>
+                </section>     
             </div>
             </>
         );
