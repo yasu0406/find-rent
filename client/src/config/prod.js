f4363cb25f5fb62760570b70b80782c618a886f5
yasuhiro <yasucom4646@gmail.com>
2019-10-09 17:53:45 -0700

    Create prod.js

---
+++ client/src/config/prod.js
@@ -0,0 +1,12 @@
+// prod.js - production keys here !!
+// dev.js - don't commit this!!
+module.exports = {
+    REACT_APP_API_KEY:process.env.FIREBASE_APP_API_KEY,
+    REACT_APP_AUTH_DOMAIN:process.env.FIREBASE_APP_AUTH_DOMAIN,
+    REACT_APP_DATABASE_URL:process.env.FIREBASE_APP_DATABASE_URL,
+    REACT_APP_PROJECT_ID:process.env.FIREBASE_APP_PROJECT_ID,
+    REACT_APP_STORAGE_BUCKET:process.env.FIREBASE_APP_STORAGE_BUCKET,
+    REACT_APP_MESSAGING_SENDER_ID:process.env.FIREBASE_APP_MESSAGING_SENDER_ID,
+    REACT_APP_APP_ID:process.env.FIREBASE_APP_APP_ID
+};
+
