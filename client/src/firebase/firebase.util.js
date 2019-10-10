f7de1ae346172ecafc2e160f7ff5c6cfcba2549e
yasuhiro <yasucom4646@gmail.com>
2019-10-09 17:37:51 -0700

    Changed settings

--- client/src/firebase/firebase.util.js
+++ client/src/firebase/firebase.util.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import keys from '../config/keys';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,  
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: keys.REACT_APP_API_KEY,
+    authDomain: keys.REACT_APP_AUTH_DOMAIN,
+    databaseURL: keys.REACT_APP_DATABASE_URL,  
+    projectId: keys.REACT_APP_PROJECT_ID,
+    storageBucket: keys.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: keys.REACT_APP_MESSAGING_SENDER_ID,
+    appId: keys.REACT_APP_APP_ID
 }
 
 firebase.initializeApp(config);
