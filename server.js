8f5382d2faf14aa45a2d7b9f86b9a9b059819d74
yasuhiro <yasucom4646@gmail.com>
2019-10-10 00:49:11 -0700

    try to fix

--- server.js
+++ server.js
@@ -28,5 +28,6 @@ if (process.env.NODE_ENV === 'production') {
     });
   }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
