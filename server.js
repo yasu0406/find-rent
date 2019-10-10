305ed33319031ad865f47fd33ec80722e0e8e8d2
yasuhiro <yasucom4646@gmail.com>
2019-10-10 00:07:27 -0700

    try to fix heroku err

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 require('./routes/roomsRoute')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-}
+  }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
