d22d5b69ff838caa1ff3aaf2c87a7c5af07498fb
yasuhiro <yasucom4646@gmail.com>
2019-10-10 01:16:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 require('./routes/roomsRoute')(app);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.static(__dirname, 'client/build')));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     const path = require('path');
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+//   }
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
