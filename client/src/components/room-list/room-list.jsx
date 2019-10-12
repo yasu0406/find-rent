a2c34e4ba3bd3ff571ff75c06b44a80581330709
yasuhiro <yasucom4646@gmail.com>
2019-10-11 19:15:21 -0700

    Updated

--- client/src/components/room-list/room-list.jsx
+++ client/src/components/room-list/room-list.jsx
@@ -10,9 +10,62 @@ const RoomList = (props) => {
     useEffect(()=> {
         props.fetchRooms();
     },[]);
-    return (
-        <div className='col-12'>Loading</div>
-    )
+    if(props.rooms.rooms) {
+        if(props.category) {
+            return props.rooms.rooms.filter((room) => props.category == room.area).filter((room, idx) => idx < 4).map(room => {
+                const title = room.title;
+                const sliceTitle = title.length > 30 ? title.slice(0, 30) + '…' : title;
+                return (
+                    <li className='col-md-3' key={room._id}>
+                        <Link to={`room-detail/${room._id}`}>
+                            <div className='thumb-nail' style={{backgroundImage: `url(${room.img1 ? room.img1: process.env.PUBLIC_URL + '/images/default-img.png'})`}}></div>
+                            <p>{room.roomSize}</p>
+                            <h3>{sliceTitle}</h3>
+                            <p>${room.price}</p>
+                        </Link>
+                    </li>
+                    )
+                }
+            )
+        } else {
+            if(props.areaCategoryName == 'all') {
+                return props.rooms.rooms.map(room => {
+                    const title = room.title;
+                    const sliceTitle = title.length > 30 ? title.slice(0, 30) + '…' : title;
+                    return (
+                        <li className='col-md-3' key={room._id}>
+                            <Link to={`room-detail/${room._id}`}>
+                                <div className='thumb-nail' style={{backgroundImage: `url(${room.img1 ? room.img1: process.env.PUBLIC_URL + '/images/default-img.png'})`}}></div>
+                                <p>{room.roomSize}</p>
+                                <h3>{sliceTitle}</h3>
+                                <p>${room.price}</p>
+                            </Link>
+                        </li>
+                        )
+                    }
+                )
+            } else {
+                return props.rooms.rooms.filter((room) => room.area.toLowerCase().includes(props.areaCategoryName)).map(room => {
+                    const title = room.title;
+                    const sliceTitle = title.length > 30 ? title.slice(0, 30) + '…' : title;
+                    return (
+                        <li className='col-md-3' key={room._id}>
+                            <Link to={`room-detail/${room._id}`}>
+                                <div className='thumb-nail' style={{backgroundImage: `url(${room.img1 ? room.img1: process.env.PUBLIC_URL + '/images/default-img.png'})`}}></div>
+                                <p>{room.roomSize}</p>
+                                <h3>{sliceTitle}</h3>
+                                <p>${room.price}</p>
+                            </Link>
+                        </li>
+                        )
+                    }
+                )
+            }
+            
+        }
+    } else {
+        
+    }
 }
 const mapStateToProps = state => {
     return {
