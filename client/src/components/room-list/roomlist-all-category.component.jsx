aba9902b071fa1ead07f3ab42fb3d651d52561da
yasuhiro <yasucom4646@gmail.com>
2019-11-13 22:45:40 -0800

    Added other address

--- client/src/components/room-list/roomlist-all-category.component.jsx
+++ client/src/components/room-list/roomlist-all-category.component.jsx
@@ -1,62 +1,40 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 import RoomList from './room-list';
 
 const RoomListAllCategory = () => {
-
+  
   return (
     <>
       <section className='container rooms-section'>
         <h2>Downtown</h2>
-        <ul className='row room-list'>
-            <RoomList category='Downtown' />
-        </ul>
-        <Link to='/category/downtown' className='btn btn-danger'>View All</Link>
+          <RoomList category='Downtown' />
         </section>
 
       <section className='container rooms-section'>
         <h2>Robson</h2>
-        <ul className='row room-list'>
-            <RoomList category='Robson' />
-        </ul>
-        <Link to='/category/robson' className='btn btn-danger'>View All</Link>
+        <RoomList category='Robson' />
       </section>
 
       <section className='container rooms-section'>
         <h2>Westend</h2>
-        <ul className='row room-list'>
-            <RoomList category='Westend' />
-        </ul>
-        <Link to='/category/westend' className='btn btn-danger'>View All</Link>
+        <RoomList category='Westend' />
       </section>
       <section className='container rooms-section'>
         <h2>EastVancouver</h2>
-        <ul className='row room-list'>
-            <RoomList category='EastVancouver' />
-        </ul>
-        <Link to='/category/eastvancouver' className='btn btn-danger'>View All</Link>
+        <RoomList category='EastVancouver' />
       </section>
       <section className='container rooms-section'>
         <h2>Kitsilano</h2>
-        <ul className='row room-list'>
-            <RoomList category='Kitsilano' />
-        </ul>
-        <Link to='/category/kitsilano' className='btn btn-danger'>View All</Link>
+        <RoomList category='Kitsilano' />
       </section>
       <section className='container rooms-section'>
         <h2>Richmond</h2>
-        <ul className='row room-list'>
-            <RoomList category='Richmond' />
-        </ul>
-        <Link to='/category/richmond' className='btn btn-danger'>View All</Link>
+        <RoomList category='Richmond' />
       </section>
       <section className='container rooms-section'>
         <h2>Burnaby</h2>
-        <ul className='row room-list'>
-            <RoomList category='Burnaby' />
-        </ul>
-        <Link to='/category/burnaby' className='btn btn-danger'>View All</Link>
+        <RoomList category='Burnaby' />
       </section>
     </>
   )
